4f9bbfd8166c16514b17aa6494fe2d53f42e29ff
fedezaff <fzaffaina@gmail.com>
2024-10-01 17:44:21 +0200

    add user class

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+// import com.example.demo.repository.UserRepository;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    // @Autowired
+    // private UserRepository userRepository;
+    
+}
