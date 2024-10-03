a165f3ff9f10338e58b48ce7bb28b05cf06b1d88
fedezaff <fzaffaina@gmail.com>
2024-10-03 16:48:13 +0200

    warning correction

--- demo/src/main/java/com/example/demo/controller/RecipeController.java
+++ demo/src/main/java/com/example/demo/controller/RecipeController.java
@@ -8,14 +8,12 @@ import com.example.demo.service.RecipeOriginsService;
 import com.example.demo.service.RecipeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
