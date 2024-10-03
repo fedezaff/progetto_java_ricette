09b0fed0ad4206298130d393ccaeb879076a8474
IFTSbelle <169815852+IFTSbelle@users.noreply.github.com>
2024-10-04 00:14:35 +0200

    added value in recipe.java, commentated jpa in app.req

--- demo/src/main/java/com/example/demo/model/Recipe.java
+++ demo/src/main/java/com/example/demo/model/Recipe.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class Recipe {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "recipe_id")  
     private Long recipeId;
 
